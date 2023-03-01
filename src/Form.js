aff04d8bcd6e11741bec54e2681df39acde14d64
Alex <rengarsanj@gmail.com>
2023-03-01 18:37:42 +0200

    DRY fix with custom component + props

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,5 @@
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage, useField } from 'formik';
 import * as Yup from 'yup';
 
 // const validate = values => {   /// Own validation. We can use this ot Yup result will be the same but Yup counts more comfortable to use.
@@ -17,6 +17,34 @@ import * as Yup from 'yup';
 //     return errors;
 // }
 
+const MyTextInput = ({label, ...props}) => { 
+    const [field, meta] = useField(props);   /// Hook from Formik to get context. Field gets id, name type e.t.c. Meta gets handleBlur handleChnage e.t.c.
+    return(
+        <>
+            <label htmlFor={props.name}>{label}</label> 
+            <input {...props} {...field}/>
+            {meta.touched && meta.error ? (
+                <div className='error'>{meta.error}</div>
+            ) : null}
+        </>
+    )
+}
+
+const MyCheckBox = ({children, ...props}) => { 
+    const [field, meta] = useField({...props, type: 'checkbox'});   
+    return(
+        <>
+            <label className='checkBox'>
+                <input type="checkbox" {...props} {...field}/>
+                {children}
+            </label> 
+
+            {meta.touched && meta.error ? (
+                <div className='error'>{meta.error}</div>
+            ) : null}
+        </>
+    )
+}
 
 const CustomForm = () => {
 
@@ -51,36 +79,42 @@ const CustomForm = () => {
         >
             <Form className="form">
                 <h2>Send a donation please ;)</h2>
-                <label htmlFor="name">your name</label>
-                <Field
+
+                {/* <label htmlFor="name">your name</label> */}
+                {/* <Field
                     id="name"
                     name="name"
                     type="text"
 
-                    /// Yhis All already setted in Field Component 
+                    /// This All already setted in Field Component 
                     // value={formik.values.name}      /// {... formik.getFieldProps('name')}  /// It can replace all 3 props   
                     // onChange={formik.handleChange}  /// {... formik.getFieldProps('name')}  /// It can replace all 3 props 
                     // onBlur={formik.handleBlur}      /// {... formik.getFieldProps('name')}  /// It can replace all 3 props 
-                />
-
+                /> */}
                 {/* {formik.errors.name && formik.touched.name ? <div className="error">{formik.errors.name}</div> : null} /// Same functional as in <ErrorMessage/> Component  */}
-                <ErrorMessage className='error' name="name" component="div"/>
+                {/* <ErrorMessage className='error' name="name" component="div"/> */}
                 {/* <ErrorMessage className='error' name="name">{msg => <div>{msg}</div>}</ErrorMessage>  /// If we dont want to use component="div"  */}
 
-                <label htmlFor="email">Your mail</label>
-                <Field
+                {/* Instead of this big construction we use MyTextInput that we created on top of the page  */}
+
+                <MyTextInput
+                    label="Your name"
+                    id="name"
+                    name="name"
+                    type="text"
+                />
+                <MyTextInput
+                    label="Your mail"
                     id="email"
                     name="email"
                     type="email"
                 />
-                <ErrorMessage className='error' name="email" component="div"/>
-                <label htmlFor="amount">Quantity</label>
-                <Field
+                <MyTextInput
+                    label="Quantity"
                     id="amount"
                     name="amount"
                     type="number"
                 />
-                <ErrorMessage className='error' name="amount" component="div"/>
                 <label htmlFor="currency">Currency</label>
                 <Field
                     id="currency"
@@ -100,14 +134,9 @@ const CustomForm = () => {
                     as="textarea"
                 />
                 <ErrorMessage className='error' name="text" component="div"/>
-                <label className="checkbox">
-                    <Field 
-                        name="terms" 
-                        type="checkbox" 
-                    />
+                <MyCheckBox name="terms">
                     Agree with the confidentiality policy?
-                </label>
-                <ErrorMessage className='error' name="terms" component="div"/>
+                </MyCheckBox>
                 <button type="submit">Отправить</button>
             </Form>
         </Formik>
